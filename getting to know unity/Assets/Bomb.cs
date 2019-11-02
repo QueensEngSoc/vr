7a4c6e284104ce84af1b5af11d99c86486ccecf1
sfwsaladbar <55550980+sfwsaladbar@users.noreply.github.com>
2019-11-02 12:35:38 -0400

    Falling Bombs

---
+++ getting to know unity/Assets/Bomb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//https://www.youtube.com/watch?v=XDAYS-qYe6Y
+
+public class Bomb : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        Respawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -10)
+            Respawn();
+
+    }
+
+    private void Respawn()
+    {
+        float randomX = UnityEngine.Random.Range(-15, 15);
+        float randomY = UnityEngine.Random.Range(10, 20);
+
+        transform.position = new Vector3(randomX, randomY);
+
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = Vector3.zero;
+    }
+}
