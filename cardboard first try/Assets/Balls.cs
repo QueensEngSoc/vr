be63dbcc57551fef3921d8a86a585b127ad2e08d
ruffoa <ruffo.alex@gmail.com>
2020-02-26 15:13:23 -0500

    WIP

--- cardboard first try/Assets/Balls.cs
+++ cardboard first try/Assets/Balls.cs
@@ -10,16 +10,28 @@ public class Balls : MonoBehaviour
     public float forceMult = 20;
     private Rigidbody rigidbody;
     int[] positions = new int[] { -1, 1, 3 };
-    
+
+    public GameObject ball;
+    private const int ballCount = 2;
+    public GameObject[] ballArr = new GameObject[ballCount];
+    public static int currBall = 0;
+
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
         rigidbody.velocity = transform.forward * - forceMult;
+
+        if (currBall == 0)
+        {
+            generateNewBall();
+        }
+
+        print("STARTING!");
     }
 
     private void OnEnable()
     {
-        Respawn();
+        //generateNewBall();
     }
 
     // Update is called once per frame
@@ -29,16 +41,35 @@ public class Balls : MonoBehaviour
         if (transform.position.z < -5)
         {
             Respawn();
+        } else if (transform.position.z < 5) {
+            //generateNewBall();
         }
 
     }
 
-    private void Respawn()
+    private void generateNewBall()
     {
         int index = UnityEngine.Random.Range(0, 2);
         int randomX = positions[index];
-        
+        //ballArr[currBall % ballCount] = ball;
+        print(currBall % ballCount + " -> " + currBall);
 
-        transform.position = new Vector3(randomX, 1, 10);
+        ballArr[currBall % ballCount] = Instantiate(ball, new Vector3(randomX, 1, 30), Quaternion.identity);
+        currBall++;
+    }
+
+    private void Respawn()
+    {
+        //int index = UnityEngine.Random.Range(0, 2);
+        //int randomX = positions[index];
+
+        var oldBall = ballArr[(currBall + 1) % ballCount];
+        currBall++;
+        generateNewBall();
+        Destroy(oldBall);
+
+        //transform.position = new Vector3(randomX, 1, 20);
+        //Destroy(this);
+        //Instantiate(ball, new Vector3(randomX, 1, 20), Quaternion.identity);
     }
 }
